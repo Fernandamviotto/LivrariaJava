b5e57eab924aef5d268cf2c9c81b6f1fd49aba84
Fernanda Martins Viotto <140853127+Fernandamviotto@users.noreply.github.com>
2024-10-25 18:52:26 -0300

    Repositorio Emprestimo

--- biblioteca/src/main/java/com/biblioteca/biblioteca/domain/repository/IEmprestimoRepository.java
+++ biblioteca/src/main/java/com/biblioteca/biblioteca/domain/repository/IEmprestimoRepository.java
@@ -1,5 +1,9 @@
 package com.biblioteca.biblioteca.domain.repository;
 
-public class IEmprestimoRepository {
-    
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.biblioteca.biblioteca.domain.entity.Emprestimo;
+
+public interface IEmprestimoRepository extends JpaRepository<Emprestimo, Long> {
+
 }
