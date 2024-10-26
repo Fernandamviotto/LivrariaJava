d4ce3cc5e30ab40f703f54af533b1e563db6a69b
Fernanda Martins Viotto <140853127+Fernandamviotto@users.noreply.github.com>
2024-10-25 22:16:06 -0300

    Trabalho adiantado

--- biblioteca/src/main/java/com/biblioteca/biblioteca/domain/entity/Emprestimo.java
+++ biblioteca/src/main/java/com/biblioteca/biblioteca/domain/entity/Emprestimo.java
@@ -1,5 +1,45 @@
 package com.biblioteca.biblioteca.domain.entity;
 
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "emprestimo")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
 public class Emprestimo {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Usando auto increment
+    private Long id;
+
+    @Column(name = "data_emprestimo", nullable = false)
+    private LocalDate dataemprestimo;
+
+    @Column(name = "data_devolucao_prevista", nullable = false)
+    private LocalDate dataDevolucaoPrevista;
+
+    @Column(name = "data_devolucao_realizada", nullable = false)
+    private LocalDate dataDevolucaoRealizada;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(name = "multa_aplicada", nullable = false)
+    private Double multaAplicada;
 }
